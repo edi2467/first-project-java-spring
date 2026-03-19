bc5df71113430d8c265e906b51cc9f97f7699e18
edi2467 <edirinnuhu123@gmail.com>
2026-03-19 15:39:09 +0100

    Initial commit

---
+++ first-project-java-spring/src/test/java/pl/edu/vistula/first_project_java_spring/FirstProjectJavaSpringApplicationTests.java
@@ -0,0 +1,14 @@
+package pl.edu.vistula.first_project_java_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FirstProjectJavaSpringApplicationTests {
+
+
+	public static void main(String[] args) {SpringApplication.run(FirstProjectJavaSpringApplication.class, args);}
+
+
+}
